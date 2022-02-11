8cb506b28d63aa4163f75ee1540b6a842a4118f8
MulloaV <mulloa0179@e-camp.cl>
2022-02-11 00:09:27 -0300

    React API Conf

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do 
-        origin 'localhost:3000',
-        resource '*',headers: :any, methods: %i[get post patch put delete]
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post patch put delete]
     end
 end
